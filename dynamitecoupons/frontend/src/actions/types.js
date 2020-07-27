9b62fc97a0024776b1c9040268af572c29fb5755
Ivan Kovac <ivan.kovac995@gmail.com>
2020-07-27 17:37:10 +0200

    Private Routes

--- dynamitecoupons/frontend/src/actions/types.js
+++ dynamitecoupons/frontend/src/actions/types.js
@@ -3,3 +3,6 @@ export const DELETE_COUPON = "DELETE_COUPON";
 export const ADD_COUPON = "ADD_COUPON";
 export const GET_ERRORS = "GET_ERRORS";
 export const CREATE_MESSAGE = "CREATE_MESSAGE";
+export const USER_LOADING = "USER_LOADING";
+export const USER_LOADED = "USER_LOADED";
+export const AUTH_ERROR = "AUTH_ERROR";
--- dynamitecoupons/frontend/src/reducers/index.js
+++ dynamitecoupons/frontend/src/reducers/index.js
@@ -2,15 +2,11 @@ import { combineReducers } from "redux";
 import coupons from "./coupons";
 import errors from "./errors";
 import messages from "./messages";
-/*
 import auth from "./auth";
-*/
 
 export default combineReducers({
   coupons,
   errors,
   messages,
-  /*
   auth,
-  */
 });
